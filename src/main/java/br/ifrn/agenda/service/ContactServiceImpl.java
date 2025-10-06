73b20cd2a6eaed281fce514791c54d9c94a00eae
Gabriel Meireles <meireles.a@escolar.ifrn.edu.br>
2025-10-06 11:46:36 -0300

    adjemda

--- src/main/java/br/ifrn/agenda/service/ContactServiceImpl.java
+++ src/main/java/br/ifrn/agenda/service/ContactServiceImpl.java
@@ -1,6 +1,7 @@
 package br.ifrn.agenda.service;
 
 import br.ifrn.agenda.model.dto.ContatoDTO;
+import br.ifrn.agenda.repository.ContactJpaRepository;
 import org.springframework.stereotype.Service;
 
 import  java.util.ArrayList;
@@ -9,17 +10,20 @@ import java.util.List;
 @Service
 public class ContactServiceImpl implements ContactService {
 
+    private final ContactRepository contactRepository;
+
+    public ContactServiceImpl(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
     @Override
     public List<ContatoDTO> getAll() {
-        List<ContatoDTO> contatoDTOS = new ArrayList<ContatoDTO>();
-            contatoDTOS.add(ContatoDTO.builder()
-                    .nome("Joao Pedro")
-                    .telefone("123456789")
-                    .build());
-            contatoDTOS.add(ContatoDTO.builder()
-                    .nome("Maria Silva")
-                    .telefone("987654321")
-                    .build());
-        return contatoDTOS;
+
+        List<ContatoDTO> contatosDTOS = contactRepository.findAll();
+
+        return contatosDTOS;
+
+
+
     }
 }
